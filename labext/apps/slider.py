44d707f1b88a847ea0b3d1af26dededb78f88a50
Binh Vu <binh@toan2.com>
2021-01-13 13:03:47 -0800

    update slider to add index jumper

--- labext/apps/slider.py
+++ labext/apps/slider.py
@@ -19,8 +19,10 @@ def slider(fn: Callable[[int], Any], min=0, max=10, step=1, clear_output: bool=T
     clear_output: bool
         clear the output before each function call
     """
+    # an output container
+    output = widgets.Output()
 
-    # define navigating buttons and slider
+    # define navigating buttons, slider, and index jumper
     prev_btn = widgets.Button(
         description='Previous',
         disabled=False,
@@ -38,32 +40,51 @@ def slider(fn: Callable[[int], Any], min=0, max=10, step=1, clear_output: bool=T
     slider = widgets.IntSlider(value=min, min=min, max=max, step=step, continuous_update=False)
     slider.layout.margin = "0 0 0 8px"
 
-    container = widgets.HBox([prev_btn, next_btn, slider])
-    output = widgets.Output()
+    jumper = widgets.Text(
+        value=str(min),
+        description=f'Index [{slider.min}, {slider.max}]',
+        disabled=False
+    )
 
-    display(container, output)
+    # define functions to interactive update the index
+    def update_index(index: int):
+        if index < slider.min or index > slider.max:
+            print(f"[ERROR] Invalid index for the slider. Get {index} while range is [{slider.min}, {slider.max}]")
+            return
+        next_btn.disabled = index == slider.max
+        prev_btn.disabled = index == slider.min
+        slider.value = index
+        jumper.value = str(index)
 
     def navigate(btn):
         with output:
             if btn == next_btn:
-                slider.value += 1
+                update_index(slider.value + 1)
             elif btn == prev_btn:
-                slider.value -= 1
+                update_index(slider.value - 1)
 
-            next_btn.disabled = slider.value == slider.max
-            prev_btn.disabled = slider.value == slider.min
-
-    def on_change(change):
+    def on_slider_change(change):
         with output:
             if clear_output:
                 output.clear_output()
-            next_btn.disabled = change['new'] == slider.max
-            prev_btn.disabled = change['new'] == slider.min
+            update_index(change['new'])
             fn(change['new'])
+
+    def on_jumper_edit(change):
+        jumper.value = "".join((c for c in change['new'] if c.isdigit()))
+
+    def on_jumper_change(_sender):
+        if jumper.value != "":
+            update_index(int(jumper.value))
 
     next_btn.on_click(navigate)
     prev_btn.on_click(navigate)
-    slider.observe(on_change, names='value')
+    slider.observe(on_slider_change, names='value')
+    jumper.observe(on_jumper_edit, names='value')
+    jumper.on_submit(on_jumper_change)
+
+    container = widgets.HBox([prev_btn, next_btn, slider, jumper])
+    display(container, output)
 
     with output:
         fn(slider.value)
